08352e65636b47e48309d61bb8a82571f8bd197f
Salvatore Criscione <salvatore.n@grrlz.net>
2025-03-29 20:58:24 +0200

    nvim colorscheme

--- dot_config/nvim/lua/custom/plugins/colorscheme.lua
+++ dot_config/nvim/lua/custom/plugins/colorscheme.lua
@@ -3,11 +3,10 @@ return {
     'neanias/everforest-nvim',
     version = false,
     lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- Optional; default configuration will be used if setup isn't called.
+    priority = 1000,
     config = function()
       require('everforest').setup {
-        -- Your config here
+        background = 'hard',
         transparent_background_level = 2,
       }
 
